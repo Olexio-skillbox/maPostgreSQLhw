835c668e0d347c49a065a41efff73317365364c0
A.Lapshin <polick@mail.ru>
2024-03-22 22:26:09 +0200

    02 Like a Busines Logic

---
+++ src/main/java/org/example/namesdb/user/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package org.example.namesdb.user.repository;
+
+import org.example.namesdb.user.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+}
